ef52a58140351942baa607208d2df5bf62be56db
aarons-daniel <91625043+aarons-daniel@users.noreply.github.com>
2022-01-01 12:27:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>APARTMENT MANAGEMENT SYSTEM</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+  <div class="main">
+    <img src="images/home.jpg" alt="apartment">
+
+    <div class="overlay"></div>
+    <div class="navbar">
+      <div class="logo">SUNSET SQUARE</div>
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="php/about.php">ABOUT US</a></li>
+        <li><a href="php/login.php">LOGIN</a></li>
+        <li><a href="php/contact_us.php">CONTACT US</a></li>
+      </ul>
+    </div>
+    <div class="heading">
+      <h1 class="head">WELCOME TO OUR <span>WEBSITE</span></h1>
+      <br><br>
+      <h3 class="sub"> A house is made of walls and beams<br> A home is built with love and
+        dreams.</h3>
+    </div>
+  </div>
+</body>
+
+</html>
